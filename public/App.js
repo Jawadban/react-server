ddd8f7053e6c66f5b07c4d1891689a044d1c5022
Jawadban <jawadbanga@gmail.com>
2017-04-22 23:55:55 -0700

    adding redux

--- public/App.js
+++ public/App.js
@@ -1,3 +1,17 @@
 import React, {propTypes} from 'react'
 import { connect } from 'react-redux'
-import { CounterApp } from './actions'
+
+import { CounterApp } from './actions' 
+
+class App extends React.Component {
+	constructor (props) {
+		super (props)
+	}
+	render () {
+		return (
+			<div>
+				<h1>Hello React</h1>
+			</div>
+		)
+	}
+}
